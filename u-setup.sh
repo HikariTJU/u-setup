affe356a95ccd0306e51c6b7077b1229df005fd2
ementt <nameless11@163.com>
2022-04-29 17:43:13 +0800

    tmux

--- u-setup.sh
+++ u-setup.sh
@@ -1,8 +1,11 @@
 sudo apt update
 curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
 
-sudo apt install -y zsh wget curl build-essential python3-pip python3 nodejs
+sudo apt install -y zsh wget curl build-essential python3-pip python3 nodejs tmux
 
+cp .tmux.conf ~/.tmux.conf
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+bash ~/.tmux/plugins/tpm/bin/install_plugins
 #install zsh
 echo "y" | sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
@@ -12,5 +15,7 @@ wget https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.deb
 sudo dpkg -i nvim-linux64.deb
 cp .zshrc ~/.zshrc
 
+yarn config set proxy $http_proxy
+yarn config set https-proxy $https_proxy
 npm install -g yarn
 bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh) --install-dependencies -y
