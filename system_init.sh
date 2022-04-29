2c24e8ef24b6df404ca8cc77ca27eee95edf83f2
ementt <nameless11@163.com>
2022-04-29 12:59:38 +0800

    second

--- system_init.sh
+++ system_init.sh
@@ -1,10 +1,14 @@
 sudo apt update
-sudo apt install -y zsh wget curl build-essentials
+sudo apt install -y zsh wget curl build-essential python3-pip python3
+curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+#install zsh
 echo "y" | sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-echo "1" | sh -c "$(curl -fsSL https://sh.rustup.rs)"
+sh -c "$(curl -fsSL https://sh.rustup.rs)"
 cargo install du-dust exa sd
 wget https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.deb
 sudo dpkg -i nvim-linux64.deb
 cp .zshrc ~/.zshrc
-bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh)
+bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh) --install-dependencies
